0438a6a5b3b75c11d4af58040f5281fd4ae76cb5
dmknapp2385 <dmknapp2385@gmail.com>
2022-03-09 12:50:49 -0700

    created manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const webpack = require('webpack');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const webpackBundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const WebpackPWaManifest = require('webpack-pwa-manifest');
+
 module.exports = {
   entry: {
     app: './assets/js/script.js',
@@ -41,8 +43,23 @@ module.exports = {
           $:"jquery",
           jQuery: "jquery"
       }),
-      new BundleAnalyzerPlugin({
+      new webpackBundleAnalyzerPlugin({
         analyzerMode: 'static', // report outputs to an HTML file in the dist folder
+      }),
+      new WebpackPWaManifest({
+        name: "Food Event",
+        short_name: "Foodies",
+        description: "An app that allows you to view upcoming food events.",
+        start_url: "../index.html",
+        background_color: "#01579b",
+        theme_color: "#ffffff",
+        fingerprints: false,
+        inject: false,
+        icons: [{
+          src: path.resolve("/assets/img/icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons")
+        }]
       })
   ],
   mode: 'development',
